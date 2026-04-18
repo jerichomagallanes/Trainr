d74bc2be473183abe258dd37755dafe5a7b8e202
jericho <jericho.magallanes324@gmail.com>
2026-04-18 14:10:05 +0800

    fix: update outdated libraries

--- app/src/main/java/com/jericx/trainr/presentation/onboarding/components/layout/OnboardingTopbar.kt
+++ app/src/main/java/com/jericx/trainr/presentation/onboarding/components/layout/OnboardingTopbar.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -34,7 +34,7 @@ fun OnboardingTopBar(
         navigationIcon = {
             IconButton(onClick = onBackClick) {
                 Icon(
-                    Icons.Default.ArrowBack,
+                    Icons.AutoMirrored.Filled.ArrowBack,
                     contentDescription = stringResource(R.string.back),
                     tint = MaterialTheme.colorScheme.onBackground
                 )
